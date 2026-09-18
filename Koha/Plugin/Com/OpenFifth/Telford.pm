73477e8de369920c9627239b0b06071bbda4830e
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-09-18 12:35:06 +0100

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/Telford.pm
+++ Koha/Plugin/Com/OpenFifth/Telford.pm
@@ -20,7 +20,7 @@ use Mojo::JSON qw(encode_json decode_json);
 
 use Koha::Plugin::Com::OpenFifth::Telford::Format;
 
-our $VERSION         = '0.1.3';
+our $VERSION         = '0.1.4';
 our $MINIMUM_VERSION = "24.11.00.000";
 
 our $metadata = {
